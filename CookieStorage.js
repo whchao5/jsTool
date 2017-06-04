ce638a5fe444d3c4ff770f4f8759ea1f35b7ca94
FeiJiHQDa <feijihqda@126.com>
2017-06-04 23:46:24 +0800

    es6 判断类型函数

--- CookieStorage.js
+++ CookieStorage.js
@@ -2,3 +2,12 @@
  * Created by W on 2017/6/4.
  * cookie 的相关操作， get, put, remove, clear,
  */
+
+/*
+** 实现像 localStorage 和 sessionStorage 一样的存储方式
+*  maxage 最大的存储时间
+*  path 路径
+ */
+function CookieStorage(maxage, path) {
+    
+}
