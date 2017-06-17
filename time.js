a18fa8b90b2f64685ad3643d670e7fca2c47727d
wuhaichao <feijihqda@126.com>
2017-06-17 16:52:59 +0800

    add time

--- time.js
+++ time.js
@@ -6,14 +6,48 @@
  * 获得当前时间
  * @returns {*}
  */
-function getDate() {
-    // 比如需要这样的格式 yyyy-MM-dd hh:mm:ss
-    var date = new Date();
-    Y = date.getFullYear() + '-';
-    M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-    D = date.getDate() + ' ';
-    h = date.getHours() + ':';
-    m = date.getMinutes() + ':';
-    s = date.getSeconds();
-    return Y + M + D + h + m + s; //呀麻碟
+'use strict';
+function Dates() {
+
+    var date = (function() {
+        return new Date(time);
+    }());
+
+    // 返回天数
+    this.getData = function (time) {
+        // 比如需要这样的格式 yyyy-MM-dd hh:mm:ss
+
+        if (typeof time !== "undefined" || !time) {
+            date = new Date(time);
+        }
+
+        var Y = date.getFullYear() + '-';
+        var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+        var D = date.getDate() + ' ';
+        var h = date.getHours() + ':';
+        var m = date.getMinutes() + ':';
+        var s = date.getSeconds();
+        return Y + M + D + h + m + s;
+    };
+
+    // 获取当前时间
+    this.now = function () {
+
+        if (!Date.now()) {
+            Date.now = function () {
+                return (new Date()).getTime();
+            }
+        }
+
+        return String(Date.now()).substring();
+    };
+
+    // YY-mm .... 转 时间戳
+    this.dateToTime = function (strtime) {
+        if (typeof strtime === "string" && strtime) {
+            var date = new Date(strtime);
+            return Number(String(date.getTime()).substring(0, 10));
+        }
+        return null;
+    }
 }
