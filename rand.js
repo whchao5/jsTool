4a632bd458b581f5a1e857533623b36f7ed72b0c
FeiJiHQDa <feijihqda@126.com>
2017-10-10 14:26:26 +0800

    Update rand.js

--- rand.js
+++ rand.js
@@ -1,16 +1,16 @@
-        //生成从minNum到maxNum的随机数
-        function randomNum(min, max) {
-            min = Number(min);
-            max = Number(max);
-            return parseInt(Math.random()*(max-min+1)+min,10);
-        }
+//生成从minNum到maxNum的随机数
+function randomNum(min, max) {
+    min = Number(min);
+    max = Number(max);
+    return parseInt(Math.random()*(max-min+1)+min,10);
+}
 
-        // 生成 一串字符串随机字符串
-        function generate_fun(length = 9) {
-            var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-            var password = '';
-            for (var i = 0; i < length; i++) {
-                password += chars[parseInt(Math.random()*(chars.length+1), 10)]
-            }
-            return password;
-        }
+// 生成 一串字符串随机字符串
+function generate_fun(length = 9) {
+    var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var password = '';
+    for (var i = 0; i < length; i++) {
+        password += chars[parseInt(Math.random()*(chars.length+1), 10)]
+    }
+    return password;
+}
